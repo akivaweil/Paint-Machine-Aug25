2085b56e04d9c59b7131a63826289899140834ef
akivaweil <akivaweil@gmail.com>
2025-07-29 00:07:59 -0700

    Fix include paths and update fallback values

--- src/StepperMotor.cpp
+++ src/StepperMotor.cpp
@@ -1,5 +1,6 @@
 #include "StateMachine/FUNCTIONS/StepperMotor.h"
 #include "config/Homing_Config.h"
+#include "config/Config.h"
 
 //* ************************************************************************
 //* ************************ STEPPER MOTOR IMPLEMENTATION ******************
