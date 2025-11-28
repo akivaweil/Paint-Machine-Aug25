d74fd64220efe95857126e5d17ee081433d1b01a
akivaweil <akivaweil@gmail.com>
2025-11-27 16:17:08 -0800

    Config: Double X_STEPS_PER_INCH to 508

--- src/config/Config.h
+++ src/config/Config.h
@@ -19,7 +19,7 @@
 #define STEPS_PER_INCH 254         // Steps per inch (400 steps / 1.575 inches per rev = 254 steps/inch) - Default for Y, Fork, Storage
 
 // X-axis motor specific steps per inch (if different from default)
-#define X_STEPS_PER_INCH 254      // Steps per inch for X motor
+#define X_STEPS_PER_INCH 508      // Steps per inch for X motor
 
 // Speed and acceleration settings (in steps per second)
 #define MAX_SPEED 5000              // Maximum speed in steps per second
