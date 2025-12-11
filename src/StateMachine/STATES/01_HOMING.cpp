6fee08c92572dc03c5eb7cfe183ef219e02d41bf
akivaweil <akivaweil@gmail.com>
2025-12-10 21:46:17 -0800

    Switch storage motor direction

--- src/StateMachine/STATES/01_HOMING.cpp
+++ src/StateMachine/STATES/01_HOMING.cpp
@@ -195,7 +195,7 @@ void homeAllAxes(bool resetColumn) {
     
     // Start storage motor clockwise to find nearest column (only on first boot and if not already at a column)
     if (motorStorage && resetColumn && !storageHomed) {
-        motorStorage->startContinuous(true);
+        motorStorage->startContinuous(false);
     }
     
     // Keep moving until all home switches/sensors are triggered
